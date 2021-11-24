89c03280f0cb515f35cefb0700facddd7f8645e3
Harsh Pandit <94526115+Unknownvip@users.noreply.github.com>
2021-11-24 16:58:48 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -56,8 +56,8 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["vplay", f"vplay@{BOT_USERNAME}"]) & other_filters)
-async def vplay(c: Client, m: Message):
+@Client.on_message(command(["audioplay", f"stream@{BOT_USERNAME}"]) & other_filters)
+async def play(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
     keyboard = InlineKeyboardMarkup(
