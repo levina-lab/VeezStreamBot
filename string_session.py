19003663ffc578b48e61783c96a6f1e2dd426e05
The Guy <92889761+Tronser@users.noreply.github.com>
2021-12-29 12:00:23 +0530

    Create string_session.py

---
+++ string_session.py
@@ -0,0 +1,18 @@
+
+#only for new users who are facing problem, program session generator
+
+from pyrogram import Client as c
+
+API_ID = input("\nEnter Your API_ID:\n > ")
+API_HASH = input("\nEnter Your API_HASH:\n > ")
+
+print("\n\n Enter Phone number when asked.\n\n")
+
+i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
+
+with i:
+    i.storage.SESSION_STRING_FORMAT=">B?256sQ?"
+    ss = i.export_session_string()
+    print(f"here is your program session")
+    print(f"\n{ss}\n")
+    
